00bcac05c2cabf4866faeae9a2193408f81f0ba0
Felix Ruhnow <felix.ruhnow@gmail.com>
2019-07-27 11:05:50 +0200

    Fixing misused header error popping up.

--- Jellyfin.Plugin.Slack/Api/ServerApiEntryPoints.cs
+++ Jellyfin.Plugin.Slack/Api/ServerApiEntryPoints.cs
@@ -50,7 +50,6 @@ namespace Jellyfin.Plugin.Slack.Api
             {
                 Url = options.WebHookUrl,
                 RequestContent = _serializer.SerializeToString(parameters),
-                RequestHeaders = {[HeaderNames.ContentType] = "application/json"}
             };
 
             await _httpClient.Post(httpRequest).ConfigureAwait(false);
